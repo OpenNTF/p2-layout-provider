aea4b36e7253db0a35dbca9c0732a71a1e29ab28
Jesse Gallagher <jesse@secondfoundation.org>
2019-12-26 18:30:41 -0500

    Add support for uncompressed and XZ-compressed artifacts.xml (Issue #3)

--- p2-layout-provider/src/main/java/org/openntf/maven/p2/model/P2Repository.java
+++ p2-layout-provider/src/main/java/org/openntf/maven/p2/model/P2Repository.java
@@ -1,13 +1,17 @@
 package org.openntf.maven.p2.model;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.jar.JarInputStream;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import org.apache.commons.compress.compressors.CompressorException;
+import org.apache.commons.compress.compressors.CompressorStreamFactory;
+import org.apache.commons.compress.compressors.lzma.LZMACompressorInputStream;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
@@ -38,32 +42,60 @@ public class P2Repository {
 	 */
 	public synchronized List<P2Bundle> getBundles() {
 		if(this.bundles == null) {
-			URI artifactsJar = URI.create(PathUtil.concat(this.uri.toString(), "artifacts.jar", '/')); //$NON-NLS-1$
+			this.bundles = new ArrayList<>();
 			
 			try {
-				Document xml;
+				URI artifactsXml = URI.create(PathUtil.concat(this.uri.toString(), "artifacts.xml", '/')); //$NON-NLS-1$
+				try(InputStream is = artifactsXml.toURL().openStream()) {
+					collectBundles(is, this.bundles);
+				} catch(FileNotFoundException e) {
+					// artifacts.xml is not present
+				}
+				
+				URI artifactsXz = URI.create(PathUtil.concat(this.uri.toString(), "artifacts.xml.xz", '/')); //$NON-NLS-1$
+				try(InputStream is = artifactsXz.toURL().openStream()) {
+					try(InputStream xis = CompressorStreamFactory.getSingleton().createCompressorInputStream(CompressorStreamFactory.getXz(), is)) {
+						collectBundles(xis, this.bundles);
+					}
+				} catch(FileNotFoundException e) {
+					// artifacts.xml.xz is not present
+				}
+				
+				URI artifactsJar = URI.create(PathUtil.concat(this.uri.toString(), "artifacts.jar", '/')); //$NON-NLS-1$
 				try(InputStream is = artifactsJar.toURL().openStream()) {
 					try(JarInputStream jis = new JarInputStream(is)) {
 						jis.getNextEntry();
-						xml = DOMUtil.createDocument(jis);
+						collectBundles(jis, this.bundles);
 					}
+				} catch(FileNotFoundException e) {
+					// artifacts.jar is not present
 				}
-				this.bundles = Stream.of(DOMUtil.nodes(xml, "/repository/artifacts/artifact[@classifier=\"osgi.bundle\"]")) //$NON-NLS-1$
-					.map(Element.class::cast)
-					.filter(el -> {
-						try {
-							return DOMUtil.nodes(el, "processing").length == 0; //$NON-NLS-1$
-						} catch (XMLException e) {
-							throw new RuntimeException(e);
-						}
-					})
-					.map(P2Bundle::new)
-					.collect(Collectors.toList());
-			} catch(IOException | XMLException e) {
+				
+				
+			} catch(IOException | XMLException | CompressorException e) {
 				throw new RuntimeException(e);
 			}
 		}
 		return this.bundles;
 	}
 
+	// *******************************************************************************
+	// * Internal implementation methods
+	// *******************************************************************************
+	
+	private static void collectBundles(InputStream is, List<P2Bundle> bundles) throws XMLException {
+		Document artifactsXml = DOMUtil.createDocument(is);
+		Stream.of(DOMUtil.nodes(artifactsXml, "/repository/artifacts/artifact[@classifier=\"osgi.bundle\"]")) //$NON-NLS-1$
+			.map(Element.class::cast)
+			.filter(el -> {
+				try {
+					return DOMUtil.nodes(el, "processing").length == 0; //$NON-NLS-1$
+				} catch (XMLException e) {
+					throw new RuntimeException(e);
+				}
+			})
+			.map(P2Bundle::new)
+			.forEach(bundles::add);
+	}
+	
 }
