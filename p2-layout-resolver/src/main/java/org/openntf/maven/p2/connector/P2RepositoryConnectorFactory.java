dbaed2c232ed595c29b2951452a858153b6ab965
Jesse Gallagher <jesse@secondfoundation.org>
2024-08-23 09:51:00 -0400

    Bump copyright year

--- p2-layout-resolver/src/main/java/org/openntf/maven/p2/connector/P2RepositoryConnectorFactory.java
+++ p2-layout-resolver/src/main/java/org/openntf/maven/p2/connector/P2RepositoryConnectorFactory.java
@@ -1,5 +1,5 @@
-/**
- * Copyright © 2019-2023 Contributors to the P2 Layout Resolver Project
+/*
+ * Copyright © 2019-2024 Contributors to the P2 Layout Resolver Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
